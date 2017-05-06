1dbf915358264da4f13fc0c874491ff06192f9f2
Joyliu290 <joyliu290@hotmail.com>
2017-05-05 20:55:12 -0400

    Android App that integrated RESTful API to generate and display random cat pictures from a website.

--- app/src/main/java/com/example/joyli/picassolibrarysample/MainActivity.java
+++ app/src/main/java/com/example/joyli/picassolibrarysample/MainActivity.java
@@ -3,23 +3,17 @@ package com.example.joyli.picassolibrarysample;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Xml;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-
+import android.util.Log;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 import com.squareup.picasso.Picasso;
+import stanford.androidlib.xml.*;
+import android.view.*;
+import android.widget.*;
+import org.json.*;
+import stanford.androidlib.*;
 
-import org.json.JSONObject;
-
-import static java.lang.System.load;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SimpleActivity {
 
     //private static final String = "https://joyyliu.files.wordpress.com/";
 
@@ -33,17 +27,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickMeClick (View view){
+GridLayout gridLayout = $(R.id.grid);
+        gridLayout.removeAllViews();
 
-        ImageView img = (ImageView) findViewById(R.id.catPhoto);
+       ImageView img = (ImageView) findViewById(R.id.catPhoto);
         img.getLayoutParams().width=550;
         Picasso.with(this)
                 .load("https://joyyliu.files.wordpress.com/2017/05/img_1620.jpg?w=640")
+                .resize(300,300)
                 .into(img);
 
         ImageView img2 = (ImageView) findViewById(R.id.catPhoto2);
         Picasso.with(this)
                 .load("https://joyyliu.files.wordpress.com/2016/07/img_1609.jpg")
                 .into(img2);
+
 
         Ion.with(this)
                 .load("http://thecatapi.com/api/images/get?format=xml&size=med&results_per_page=6")
@@ -52,17 +50,47 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onCompleted(Exception e, String result) {
                         //data has arrived
-
-                        try{
-                            JSONObject json = new JSONObject(result);
-                            JSONObject value = json.getJSONObject("value");
-                        } catch (JSONException e1) {
-                            e1.printStackTrace();
-                        }
+                        loadCatPics(result);
                     }
                 });
+                    }
 
+public void loadCatPics(String result){
+                        try{
+                            JSONObject json = XML.toJSONObject(result);
+                            log(json);
+                       JSONArray a = json.getJSONObject("response")
+                               .getJSONObject("data")
+                               .getJSONObject("images")
+                               .getJSONArray("image");
 
+                            for (int i =0; i<a.length(); i++)
+                            {
+                                JSONObject img4 = a.getJSONObject(i);
+                                String url = img4.getString("url");
+                                log ("loading image from " + url);
+                                loadImage(url);
+                            }
+
+                        } catch (JSONException jsone) {
+                            Log.wtf("help", jsone);
+                        }
+                    }
+
+    public void loadImage(String url) {
+        ImageView imgView = new ImageView(this);
+        ViewGroup.LayoutParams params= new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        imgView.setLayoutParams(params);
+        GridLayout gridLayout = $(R.id.grid);
+        gridLayout.addView(imgView);
+
+        Picasso.with(this)
+                .load(url)
+                .resize(300,300)
+                .into(imgView);
     }
 
 }
